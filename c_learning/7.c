81ad4d625e037e9e001be3cd066eb93d93c0c84f
yuicchicode <yuicchisama@gmail.com>
2024-02-24 12:06:22 -0300

    New File

---
+++ c_learning/7.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#define PI 3.14159
+/*
+
+scanf ler tipo de dados básicos
+fgets ler texto até a quebra de linha
+
+int %d ou %i 
+long int %li 
+long long int %lli 
+float %f 
+double %lf 
+char %c 
+char[] %s para ler texto contíguo. para ler até  fim da linha fgets* 
+
+pra ler o texto até a quebra de linha 
+
+        void ler_texto(char *buffer, int length) {
+         fgets(buffer, length, stdin);
+         strtok(buffer, "\n");
+        } 
+
+Para limpar buffer de entrada 
+        void limpar_entrada() {
+         char c;
+         while ((c = getchar()) != '\n' && c != EOF) {}
+        } 
+usa quando o programa já leu algum outro dado e deiuxou uma quebra de linha pendente
+
+*/
+
+void limpar_entrada() {
+ char c;
+ while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+void ler_texto(char *buffer, int length) {
+ fgets(buffer, length, stdin);
+ strtok(buffer, "\n");
+} 
+
+
+int main()
+{
+
+    int raio, teste;
+    float resultado;
+    
+    printf("Digite o valor do raio do circulo: ");
+    scanf("%i", &raio);
+    teste = pow(raio,2);
+    resultado = PI * teste;
+    printf("Area: %f", resultado);
+    
+    
+    return 0;
+}
