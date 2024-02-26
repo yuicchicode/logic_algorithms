34d8c08f95d9af8b6d0c85c3573828b2b37a6054
yuicchicode <yuicchisama@gmail.com>
2024-02-26 10:59:57 -0300

    New File

---
+++ c_learning/10.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#define PI 3.14159
+/*
+
+scanf ler tipo de dados básicos
+fgets ler texto até a quebra de linha
+
+int %d ou %i 
+long int %li 
+long long int %lli 
+float %f 
+double %lf 
+char %c 
+char[] %s para ler texto contíguo. para ler até  fim da linha fgets* 
+
+pra ler o texto até a quebra de linha 
+
+        void ler_texto(char *buffer, int length) {
+         fgets(buffer, length, stdin);
+         strtok(buffer, "\n");
+        } 
+
+Para limpar buffer de entrada 
+        void limpar_entrada() {
+         char c;
+         while ((c = getchar()) != '\n' && c != EOF) {}
+        } 
+usa quando o programa já leu algum outro dado e deiuxou uma quebra de linha pendente
+
+*/
+
+void limpar_entrada() {
+ char c;
+ while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+void ler_texto(char *buffer, int length) {
+ fgets(buffer, length, stdin);
+ strtok(buffer, "\n");
+} 
+
+
+int main()
+{
+
+    float a, b, c;
+    
+    printf("Digite a medida A: ");
+    scanf("%f", &a);
+    printf("Digite a medida B: ");
+    scanf("%f", &b);
+    printf("Digite a medida C: ");
+    scanf("%f", &c);
+    
+    printf("Area do Quadrado: %.4f", a * a);
+    printf("\nArea do Tringulo: %.4f", (a * b)/2);
+    printf("\nArea do trapezio = %.4f", ((a + b) * c)/2);    
+    
+    return 0;
+}
